9748fa03285859fcc3aa18731c5bb41ca10cf7b2
natsuki-engr <git@stackblitz.com>
2025-06-02 11:21:39 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="tsx">
+import { h } from 'vue';
+import HelloWorld from './components/HelloWorld.vue'
+
+const { message } = defineProps(['message']);
+
+const render = () => {
+  return <div>hi</div>;
+};
+</script>
+
+<template>
+  <render />
+</template>
