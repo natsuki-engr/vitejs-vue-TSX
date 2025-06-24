ac405347385d120b9035c4d6ce192371de9ec8ef
natsuki ueda <biafrajr@gmail.com>
2025-06-24 13:59:13 +0900

    add browser test configuration and HelloWorld test case

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  test: {
+    include: ["src/**/*.test.ts", "src/**/*.test.tsx"],
+    exclude: ["src/**/*.browser.test.ts", "src/**/*.browser.test.tsx"],
+  },
 });
--- vitest.browser.config.ts
+++ vitest.browser.config.ts
@@ -1,18 +1,19 @@
-import { defineConfig } from 'vitest/config'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vitest/config";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
   test: {
+    include: ["src/**/*.browser.test.ts", "src/**/*.browser.test.tsx"],
     browser: {
       enabled: true,
-      provider: 'playwright',
+      provider: "playwright",
       // https://vitest.dev/guide/browser/playwright
       instances: [
-      { browser: 'chromium' },
-      { browser: 'firefox' },
-      { browser: 'webkit' },
+        { browser: "chromium" },
+        { browser: "firefox" },
+        { browser: "webkit" },
       ],
     },
   },
-})
+});
